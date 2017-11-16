88e3fa066f2525fa9cb9234c6245d519e0f0a803
Khaya Mathebula <khayaprom@gmail.com>
2017-11-16 22:25:08 +0200

    add functionality to dynamically change answer onClick

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageBall: UIImageView!
+    var randomImage = 0
+    let imageArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func answerAction(_ sender: UIButton) {
+        updateAnswer();
+    }
+    
+    func updateAnswer() {
+        randomImage = Int(arc4random_uniform(5))
+        print("random number: \(imageArray[randomImage])")
+        imageBall.image = UIImage(named: imageArray[randomImage])
+    }
+    
 }
 
